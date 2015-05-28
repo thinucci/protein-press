d05c38e7e6679f4634ae8d8e69cac487a5085465
ticovfx <thiagonucci@gmail.com>
2015-05-28 19:16:07 -0400

    added basic calculator functionality

---
+++ js/calculator.js
@@ -0,0 +1,33 @@
+// When document is ready run the code below
+
+  $(function() {
+
+
+//CALCULATE
+    
+   $('#calculate').click(function() {
+
+	var input = $('#weight').val();
+
+   	
+   	if(input > 0) {
+
+	  	var minimum = input * 1.2;
+	  	var maximum = input * 2.2;
+	  	$('#totalIntakeMin').text(minimum + 'g' + '-');
+	  	$('#totalIntakeMax').text(maximum + 'g');
+	  	$('#invalidInput').text('');
+	}
+
+	else {
+		$('#totalIntakeMin').text('');
+	  	$('#totalIntakeMax').text('');
+		$('#invalidInput').text('Not a valid weight number.');
+	}
+    
+   });
+
+
+});
+  
+    
