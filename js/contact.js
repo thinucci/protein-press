42e05a3b80626d3545a542e225df3a5aa0057b09
ticovfx <thiagonucci@gmail.com>
2015-06-30 20:03:07 -0400

    add selector variables to contact js

--- js/contact.js
+++ js/contact.js
@@ -1,16 +1,27 @@
 $(function() {
 
+// Selectors
+
+var $fullname = $('#fullname'),
+    $fullnameError = $('#fullname-error'),
+    $email = $('#email'),
+    $emailError = $('#email-error'),
+    $message = $('#message'),
+    $messageError = $('#message-error'),
+    $title = $('#title'),
+    $titleError = $('#title-error');
+
 // Validate Contact Form
 
 function validateName(fullname){
 	if(fullname.length >= 3) {
-		$('#fullname').removeClass('error');
-		$('#fullname-error').hide();
+		$fullname.removeClass('error');
+		$fullnameError.hide();
 		return true;
 	}
 	else{
-		$('#fullname').addClass('error');
-		$('#fullname-error').show();
+		$fullname.addClass('error');
+		$fullnameError.show();
 		return false;
 	}
 }
@@ -18,13 +29,13 @@ function validateName(fullname){
   function validateEmail(email) {
     var re = /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/;
     if (re.test(email)) {
-      $('#email').removeClass('error');
-      $('#email-error').hide();
+      $email.removeClass('error');
+      $emailError.hide();
       return true;
     }
     else {
-      $('#email').addClass('error');
-      $('#email-error').show();
+      $email.addClass('error');
+      $emailError.show();
       return false;
     }
 }
@@ -32,26 +43,26 @@ function validateName(fullname){
 
 function validateMessage(message) {
     if (message.length > 0) {
-      $('#message').removeClass('error');
-      $('#message-error').hide();
+      $message.removeClass('error');
+      $messageError.hide();
       return true;
     }
     else {
-      $('#message').addClass('error');
-      $('#message-error').show();
+      $message.addClass('error');
+      $messageError.show();
       return false;
     }
   } 
 
 function validateTitle(title) {
     if (title.length > 0) {
-      $('#title').removeClass('error');
-      $('#title-error').hide();
+      $title.removeClass('error');
+      $titleError.hide();
       return true;
     }
     else {
-      $('#title').addClass('error');
-      $('#title-error').show();
+      $title.addClass('error');
+      $titleError.show();
       return false;
     }
   }
@@ -61,10 +72,10 @@ function validateTitle(title) {
 
   $('form').submit(function(event){
 
-    var fullname = $('#fullname').val(),
-        email = $('#email').val(),
-        message = $.trim($('#message').val());
-        title = $.trim($('#title').val());
+    var fullname = $fullname.val(),
+        email = $email.val(),
+        message = $.trim($message.val());
+        title = $.trim($title.val());
 
     if (validateName(fullname) & validateEmail(email) & validateMessage(message) & validateTitle(title)) {
       return true; //submit form.
