f25fb5282753f6de5ea28b29cc0a576d2e12b7e5
ticovfx <thiagonucci@gmail.com>
2015-07-02 12:30:16 -0400

    add tabs to vid panel

--- js/main.js
+++ js/main.js
@@ -28,17 +28,85 @@ $('#supplementList').find('.toggle').click(function(){
 
 ///// VIDEO CAROUSEL TABS //////////////
 
-$('#vidSection').find('.pane').click(function(){
+var $vidNut = $('.vid-nut'),
+    $vidTrain = $('.vid-train'),
+    $vidMotiv = $('.vid-motiv'),
+    $vidComp = $('.vid-comp'),
+    $tabNut = $('#tab-nut'),
+    $tabTrain = $('#tab-train'),
+    $tabMotiv = $('#tab-motiv'),
+    $tabComp = $('#tab-comp');
 
-    //Expand or collapse this panel
-    $('.vid').next().show();
+$vidNut.show();
 
-    //Hide the other panels
-    $('.vid').not($(this).next()).hide();
-    
-   });
+$tabNut.click(function(){
+    hideAll();
+    $vidNut.show();
+    removeActive();
+    $tabNut.addClass('activePane');
 
+    $('.flexslider').flexslider({
+    animation: "slide"
+  });
 
+});
 
+$tabTrain.click(function(){
+    hideAll();
+    $vidTrain.show();
+    removeActive();
+    $tabTrain.addClass('activePane');
+});
+
+$tabMotiv.click(function(){
+    hideAll();
+    $vidMotiv.show();
+    removeActive();
+    $tabMotiv.addClass('activePane');
+});
+
+$tabComp.click(function(){
+    hideAll();
+    $vidComp.show();
+    removeActive();
+    $tabComp.addClass('activePane');
+
+});
+
+function hideAll(){
+    $vidNut.hide();
+    $vidTrain.hide();
+    $vidMotiv.hide();
+    $vidComp.hide();
+};
+
+function removeActive(){
+    $tabNut.removeClass('activePane');
+    $tabTrain.removeClass('activePane');
+    $tabMotiv.removeClass('activePane');
+    $tabComp.removeClass('activePane');
+};
+
+///////////////// SLICK CAROUSEL
+
+// $('.flexslider').flexslider({
+//     animation: "slide"
+//   });
+
+// $( "#tabs" ).tabs({
+//  beforeActivate: function( e, ui ) {
+//      if(ui.oldTab.length>0){
+//         ui.oldTab.find( ".flexslider" ).flexslider( "destroy" );
+//      }
+//  },
+//  show: function(e,ui){
+//     $(ui.panel).find( ".flexslider" ).flexslider({
+//     animation: "slide",
+//     controlNav: "thumbnails",
+//     animationLoop: false,
+//     slideshow: false
+//     });
+//  }
+// });
 
 });
